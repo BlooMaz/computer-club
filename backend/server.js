1f0d2b9ddfcf998bf5d7aee5b18ecf9a3c9d95fa
BlooMaz <muhdmuaz121@gmail.com>
2023-07-01 03:35:47 +0800

    modified:   backend/server.js 	modified:   src/RegisterConfirmation.js 	modified:   src/confirmation.css

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,17 @@ const path = require('path');
 
 const cors = require('cors');
 const multer = require('multer');
+var name1;
+var IC1;
+var username1;
+var password1;
+var address1;
+var gender1;
+var email1;
+var class1;
+var age1;
+var num1;
+var interest1=[];
 
 // Create an instance of the multer middleware
 const upload = multer();
@@ -17,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/process', (req, res) => {
-    
+    res.json({"datas":[name1,IC1,username1,address1,gender1,email1,class1,age1,num1,interest1]})
   });
 app.post('/process',upload.none(),(req, res) => {
     const formData = req.body;
@@ -44,6 +55,19 @@ app.post('/process',upload.none(),(req, res) => {
     console.log('Age:', age);
     console.log('Telephone:', numphone);
     console.log('Interests:', interests);
+
+    name1 = name;
+    IC1 = IC;
+    username1 = username;
+    password1 = password;
+    address1 = address;
+    gender1 = gender;
+    email1 = email;
+    class1 = classValue;
+    age1 = age;
+    num1 = numphone;
+    interest1 = interests;
+    
 
   res.status(200).end();
 });
--- src/RegisterConfirmation.js
+++ src/RegisterConfirmation.js
@@ -2,32 +2,31 @@ import React, { useEffect, useState } from 'react';
 import './confirmation.css';
 
 const RegisterConfirmation = () => {
-  const [data, setData] = useState(null);
+  const[backendData,setBackendData] = useState([{}]);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/process');
-      const data = await response.json();
-      setData(data);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
+  useEffect(()=>{
+    fetch('http://localhost:5000/process').then(
+      response=>response.json()
+    ).then(
+      data =>{setBackendData(data)}
+    )
+  })
   return (
     <div className='box1'>
       <h1>Registration Successful!</h1>
       <p>Thank you for registering.</p>
-      {data && (
-        <div>
-          <h2>Received Data:</h2>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
-      )}
+
+      {(typeof backendData.datas === 'undefined')?(
+        <p>loading...</p>
+      ):(
+        backendData.datas.map((data,i)=>(
+          <p>{data}</p>
+        ))
+      )
+    }
+      
+        
+    
     </div>
   );
 };
